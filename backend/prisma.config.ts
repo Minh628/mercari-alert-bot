1789f7ece11163968d5ce55d0ebbcca1392ea4ef
Raymond <kienminh628@gmail.com>
2026-06-14 20:08:50 +0700

    fix deploy fail

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,6 +1,10 @@
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
+// Gán tạm chuỗi giả để vượt qua lỗi của Prisma lúc chạy "docker build" trên Render
+if (!process.env.DATABASE_URL) {
+  process.env.DATABASE_URL = "postgresql://dummy:dummy@localhost/dummy";
+}
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
